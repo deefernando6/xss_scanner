1354fe392db0ccb867c1518cf9c6d8405f431669
deeghayu <deefernando6@gmail.com>
2021-07-21 20:04:57 +0530

    Update xss_scanner.py

--- xss_scanner.py
+++ xss_scanner.py
@@ -1,3 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+import warnings, argparse, syss
