5fbe8af8c59faf04c7cd43df16d6f8b782d65557
deeghayu <deefernando6@gmail.com>
2021-07-22 22:36:41 +0530

    Update xss_scanner.py

--- xss_scanner.py
+++ xss_scanner.py
@@ -5,3 +5,10 @@ from selenium.common.exceptions import TimeoutException
 import warnings, argparse, syss
 
 warnings.filterwarnings('ignore')
+
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+options.add_argument('--disable-xss-auditor')
+options.add_argument('--disable-web-security')
+options.add_argument('--ignore-certification-errors')
+options.add_argument('--no-sandbox')
